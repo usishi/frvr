4c2a7b2cb17baf2bd5389fa054507efa353f5cd8
Fatih Soydan <fatihsoydan@fatihsoydan.com>
2013-09-24 00:35:14 +0300

    changes

--- app.js
+++ app.js
@@ -40,25 +40,29 @@ var runProject=function(prj){
 	var prm = prj.startup.split(' ');
 	var runner = prm.shift();	
 	
-	var ps=spawn(runner,prm,{cwd:prj.folder}).on('error',function(e){
+	prj.process=spawn(runner,prm,{cwd:prj.folder}).on('error',function(e){
 		console.log("error"+e);
 		fs.appendFile(Config.logfolder+'/frvr.log', newLine(prj.title)+' has got error : '+e, 'utf8');
 		setTimeout(function(){
-			jobs.push(prj);
+			if (jobs.indexOf(prj)<0){
+				jobs.push(prj);	
+			}
 		}, 1000);
 	});
-	prj.cprocess=ps;
 	prj.starttime=new Date();
 	fs.appendFile(Config.logfolder+'/frvr.log', newLine(prj.title)+' started ', 'utf8');
 
-	ps.on('exit',function(r){
+	prj.process.on('exit',function(r){
 		fs.appendFile(Config.logfolder+'/frvr.log', newLine(prj.title)+'exited with code '+r, 'utf8');
+		if (jobs.indexOf(prj)<0){
+			jobs.push(prj);	
+		}
 	});
-	ps.stdout.on('data',function(data){ 
+	prj.process.stdout.on('data',function(data){ 
 		fs.appendFile(Config.logfolder+'/frvr'+prj.id+prj.stdout, data, 'utf8');
 		prj.lastLog=data;
 	});
-	ps.stderr.on('data',function(data){
+	prj.process.stderr.on('data',function(data){
 		fs.appendFile(Config.logfolder+'/frvr'+prj.id+prj.stderr, data, 'utf8');
 		prj.lastLog=data;
 	});
@@ -103,7 +107,7 @@ http.createServer(function (req, res) {
 setInterval(function(){
 	var proj = jobs.shift();
 	try{
-		proj.kill();
+		proj.process.kill();
 	} catch(e){}
 	runProject(proj);
 },Config.restartInterval);
